039f2092ee485d2ca907663710e5e45a0b905012
Kamerron Pace <youngkamman@gmail.com>
2022-07-04 05:27:58 -0400

    first snapshot

---
+++ cart.js
@@ -0,0 +1,103 @@
+///////////////////////////////////////////////
+///////////////////CART.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code to
+    calculate order totals. You'll also be 
+    creating customer objects.  
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Below is a cart array that has food objects
+    inside. 
+
+    Write a callback below that uses the reduce
+    array method to calculate the sum of all
+    the food. 
+*/
+
+const cart = [
+    {
+        name: 'pizza', 
+        price: 9.99
+    }, 
+    {
+        name: 'pasta', 
+        price: 8.99
+    }, 
+    {
+        name: 'salad', 
+        price: 7.99
+    }
+]
+
+//CODE HERE
+
+
+const summedPrice = cart.reduce((acc, curr) => acc + curr)
+
+
+//////////////////PROBLEM 2////////////////////
+/*  
+    Write a function called `calcFinalPrice` that
+    can take in `cartTotal`,`couponValue`,
+    and `tax` arguments. 
+
+    Inside the function, calculate the tax 
+    on the cartTotal and add it in. Subtract
+    the value of the coupon. Return the final
+    number. 
+
+    Note: the numbers passed in for `tax` will be
+    decimals, for example: .06 for a 6% tax.
+*/
+
+//CODE HERE
+const calcFinalPrice = (carTotal,couponValue,tax) =>{
+   return carTotal + tax - couponValue
+}
+console.log(calcFinalPrice(500,.06,25))
+
+//////////////////PROBLEM 3////////////////////
+/*  
+    In this problem, you'll create a model for 
+    a customer object as well as an example
+    object. 
+
+    Plan out a customer object for the cart page.
+    Think about the information that a 
+    restaurant would need about its customers.
+
+    In the TEXT ANSWER area below, describe the
+    properties that your customer object will have
+    and why you chose those properties.
+
+    Explain what data types each property should be
+    and why you chose those data types. 
+
+    Your object should have at least 4 properties. 
+*/
+
+/*
+    TEXT ANSWER HERE
+ the customer will have 4 property types name,age,favoriteFood,placeinline
+  name would be a string i picked name because you need to know your customers name
+  age would be a number i picked it because it gives me a better grasp on what demographic my customers are from
+  favoritefood would be a string i choose this because knowing a customers favorite food really makes the customer feel like you care 
+  placeInLine would be a string i choose this because this would help me and the customer know where they are in the proccess if getting their food 
+*/
+
+/*
+    Now, create a customer object following your own
+    guidelines.
+*/
+
+//CODE HERE
+const customer1 = {
+    name: "Bob",
+    Age : 35,
+    favoriteFood: "Chicken",
+    placeInLine: 7
+}
